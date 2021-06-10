0b4ae5523607f05b17eb620529eb7007e5e1e733
mcostaflor <matheuscostaflor@gmail.com>
2021-06-10 18:16:44 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NonConformitiesList from './page/NonConformitiesList';
+
+function App() {
+  return (
+    <div>
+      <NonConformitiesList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/NonConformitiesList/index.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+const NonConformitiesList = () => {
+    //const [ncrList, setNcrList] = useState(50);
+
+    useEffect(() => {
+        //fetchApi
+        //setstate ncrlist
+    });
+
+    return (
+        <div>
+            <h1>
+                NonConformities
+            </h1>
+            <table>
+                <tr>
+                    <th>
+                        Id
+                    </th>
+                    <th>
+                        Description
+                    </th>
+                    <th>
+                        Ocurrence Date
+                    </th>
+                </tr>
+                {ncrList.map(ncr => {
+                    return <tr>
+                        <td>
+                            {ncr.id}
+                        </td>
+                        <td>
+                            {ncr.description}
+                        </td>
+                        <td>
+                            {ncr.ocurrenceDate}
+                        </td>
+                    </tr>
+                })}
+            </table>
+        </div>
+    );
+}
+
+export default NonConformitiesList;
